d4fa2f9f463341e1904a7417610e698234a70bf6
Gina Henderson <gina@ginahenderson.me>
2022-02-20 20:24:57 -0600

    Pass second tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeated = '';
+    if (num < 0 || typeof num !== "number") return 'ERROR';
+    for (let i = 0; i < num; i++) {
+        repeated = `${repeated}${string}`;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
