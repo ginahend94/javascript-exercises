de760007e938567829d9f12677af67114b071c43
Gina Henderson <gina@ginahenderson.me>
2022-02-21 00:39:19 -0600

    Remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    // For each item, filter array for item
+    let filteredArray = array;
+    items.forEach(item => {
+        filteredArray = filteredArray.filter(a => {
+            return item !== a;
+        });
+    });
+    return filteredArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
