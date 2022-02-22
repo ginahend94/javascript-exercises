cb63f2d176d1883df7a943da10e9cce34f622e6f
Gina Henderson <gina@ginahenderson.me>
2022-02-21 22:18:57 -0600

    begin sum function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let larger = num1 > num2 ? num1 : num2;
+    let smaller = num2 < num1? num2 : num1;
+    let sum = 0;
+    for (let x = smaller; x >= larger; x++) {
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
