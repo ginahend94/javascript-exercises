bae9e26fc3297637ecbcda429d0f6028cb0c840c
Gina Henderson <gina@ginahenderson.me>
2022-02-21 22:53:18 -0600

    Edit function to account for errors

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,9 @@
 const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') return "ERROR";
     let larger = num1 > num2 ? num1 : num2;
     let smaller = num2 < num1? num2 : num1;
     let sum = 0;
-    for (let x = smaller; x >= larger; x++) {
+    for (let x = smaller; x <= larger; x++) {
         sum += x;
     }
     return sum;
