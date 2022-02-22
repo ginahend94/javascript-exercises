df9222acc5ae894898c59bff5c9ce535d5b10619
Gina Henderson <gina@ginahenderson.me>
2022-02-21 23:14:28 -0600

    Add temp conversion formulas

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const rounded = number => Math.round(number * 10) / 10;
 
-const ftoc = function() {
-
+const ftoc = function(temp) {
+// c = (f - 32) * 5/9
+  return rounded((number - 32) * (5/9))
 };
 
-const ctof = function() {
-
+const ctof = function(number) {
+// f = c *  9/5 + 32
+  return rounded(number * (9/5) + 32)
 };
 
 // Do not edit below this line
