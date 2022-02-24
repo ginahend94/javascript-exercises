7461ecfec517423fbbb3d41b9f7e35d49ba7d5b3
Gina Henderson <gina@ginahenderson.me>
2022-02-24 09:28:28 -0600

    Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[\W+_]/g;
+    const forwards = string.toLowerCase().replace(regex, '');
+    const backwards = forwards.split('').reverse().join('');
+    return forwards == backwards;
 };
 
 // Do not edit below this line
