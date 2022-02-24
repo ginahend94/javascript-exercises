db593a988537cfeaccdcbcc09e7f8d82779d599c
Gina Henderson <gina@ginahenderson.me>
2022-02-24 09:45:23 -0600

    Fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    if (place < 0) return "OOPS";
+    // Start with 0 and 1
+    let old = 0;
+    let current = 1;
+    let fib = 0;
+    for (let i = 0; i < place; i++) {
+    // Add together
+        fib = old + current;
+    // Make 1 old num, sum new num
+        old = current;
+        current = fib;
+    }
+    // Repeat
+    return old;
 };
 
 // Do not edit below this line
